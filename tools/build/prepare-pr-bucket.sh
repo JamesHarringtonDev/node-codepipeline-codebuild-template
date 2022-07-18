713f3cd68f436144d2046b8d59d5284586b61f66
jamesharringtondev <jamesharringtondev@gmail.com>
2022-07-18 23:57:27 +0100

    update stack, codebuild iam policies updated

--- tools/build/prepare-pr-bucket.sh
+++ tools/build/prepare-pr-bucket.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
  
-if [ aws s3 ls "s3://$PR_BRANCH_NAME" | grep 'AccessDenied' &> /dev/null == 0 ]; then
+if aws s3 ls "s3://$PR_BRANCH_NAME" | grep 'AccessDenied' &> /dev/null == 0; then
   echo "$PR_BRANCH_NAME exists but but we do not have access to"
   exit
-elif [ aws s3 ls "s3://$PR_BRANCH_NAME" | grep 'NoSuchBucket' &> /dev/null == 0 ]; then
+elif aws s3 ls "s3://$PR_BRANCH_NAME" | grep 'NoSuchBucket' &> /dev/null == 0; then
   echo "Emptying contents of bucket: s3://$PR_BRANCH_NAME"
   aws s3 rm s3://$SITE_BUCKET_NAME --recursive
 else
