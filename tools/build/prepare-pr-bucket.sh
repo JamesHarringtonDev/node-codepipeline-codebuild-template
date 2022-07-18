884e8be0d0fe09fa9b45f11b17775ea837b9a8c3
jamesharringtondev <jamesharringtondev@gmail.com>
2022-07-18 23:23:01 +0100

    prepare-pr-bucket.sh updated

--- tools/build/prepare-pr-bucket.sh
+++ tools/build/prepare-pr-bucket.sh
@@ -1,10 +1,10 @@
 bucketResponse=$(aws s3 ls "s3://$PR_BRANCH_NAME") 
-
-if [ $bucketResponse | grep 'AccessDenied' &> /dev/null) == 0 ] 
+     
+if [ ($bucketResponse | grep 'AccessDenied' &> /dev/null) == 0 ] 
 then
   echo "$PR_BRANCH_NAME exists but but we do not have access to"
   exit
-elif [ $bucketResponse | grep 'NoSuchBucket' &> /dev/null) == 0 ]
+elif [ ($bucketResponse | grep 'NoSuchBucket' &> /dev/null) == 0 ]
   echo "Emptying contents of bucket: s3://$PR_BRANCH_NAME"
   aws s3 rm s3://$SITE_BUCKET_NAME --recursive
 else
